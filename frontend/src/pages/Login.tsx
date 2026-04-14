65cb83ab9cedb4730999ac33590b164ded0cbf57
SayanH-BIT <sayan.haldar.brainium@gmail.com>
2026-04-14 13:39:38 +0530

    modified registration & login logic

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,32 +1,60 @@
-import { Box, Button, Input, useToast } from "@chakra-ui/react";
+import { Box, Button, FormControl, FormErrorMessage, Input, useToast } from "@chakra-ui/react";
 import { Link, useNavigate } from "react-router-dom";
 import { MdLogin } from "react-icons/md";
 import { FaEye, FaEyeSlash } from "react-icons/fa6";
-import { useRef, useState } from "react";
-import { DUMMY_USER } from "../services/authService";
+import { useState } from "react";
 import { generateToken } from "../utils/jwt";
+import { useFormik } from "formik";
+import { SigninForm } from "../assets/types/type";
+import { loginSchema } from "../utils/validation";
+import { BASE_URL } from "../assets/api/api";
 
 const Login = ({ isLightmode }: { isLightmode: boolean }) => {
     const toast = useToast();
     const navigate = useNavigate();
     const [showPassword, setShowPassword] = useState<boolean>(false);
     const [loading, setLoading] = useState<boolean>(false);
-    const emailRef = useRef<HTMLInputElement>(null);
-    const passwordRef = useRef<HTMLInputElement>(null);
 
-    // login handler
-    const handleLogin = (e: any) => {
-        e.preventDefault();
-        const email = emailRef.current?.value || "";
-        const password = passwordRef.current?.value || "";
-        try {
-            setLoading(true);
-            const storedUser = JSON.parse(localStorage.getItem("users") || "{}");
-            if ((email === storedUser.email && password === storedUser.password) || (email === DUMMY_USER.email && password === DUMMY_USER.password)) {
+    // login
+    const formik = useFormik<SigninForm>({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        validationSchema : loginSchema,
+        onSubmit: async (values, { resetForm }) => {
+            try {
+                setLoading(true);
+                const response = await fetch(`${BASE_URL}/users?email=${values.email}&password=${values.password}`);
+                if(!response.ok){
+                    toast({
+                        title: "Invalid",
+                        status: "error",
+                        position: 'top',
+                        duration: 1000,
+                        isClosable: true,
+                    });
+                    return;
+                }
+                // const res = response.json();
+                const users = await response.json();
+                const user = users[0];
+                if(users.length === 0){
+                    toast({
+                        title: "User not found",
+                        description:"Check credentials properly",
+                        status: "error",
+                        position: 'top',
+                        duration: 1000,
+                        isClosable: true,
+                    });
+                    return;
+                }
+                console.log("user details ==>", user);
                 const token = generateToken({
-                    email,
-                    firstName: storedUser.firstName,
-                    lastName: storedUser.lastName
+                    email: user.email,
+                    firstName: user.firstName,
+                    lastName: user.lastName
                 });
                 localStorage.setItem('token', token);
                 toast({
@@ -34,34 +62,26 @@ const Login = ({ isLightmode }: { isLightmode: boolean }) => {
                     description: "You have been logged in successfully.",
                     status: "success",
                     position: 'top',
-                    duration: 3000,
+                    duration: 1000,
                     isClosable: true,
                     onCloseComplete() { navigate("/projects"); },
                 });
-            } else {
+                resetForm();
+            } catch (error: any) {
+                setLoading(true);
                 toast({
-                    title: "Login Failed",
-                    description: "Invalid credentials",
+                    title: "Error",
+                    description: error.message || "Something went wrong.",
                     status: "error",
                     position: 'top',
-                    duration: 3000,
+                    duration: 2000,
                     isClosable: true,
                 });
+            } finally {
+                setLoading(false);
             }
-        } catch (error: any) {
-            setLoading(true);
-            toast({
-                title: "Error",
-                description: error.message || "Something went wrong.",
-                status: "error",
-                position: 'top',
-                duration: 3000,
-                isClosable: true,
-            });
-        } finally {
-            setLoading(false);
         }
-    };
+    });
 
     return (
         <div className={`flex flex-col items-center justify-start gap-10 m-auto min-h-screen ${isLightmode ? 'bg-gray-100 text-black' : 'bg-gray-900 text-white'}`}>
@@ -69,26 +89,43 @@ const Login = ({ isLightmode }: { isLightmode: boolean }) => {
                 <p className="font-semibold text-2xl">Sign In</p>
 
                 {/* login form */}
-                <form className={`h-full w-full bg-transparent gap-4 px-5 py-0 flex flex-col items-start justify-center`}>
-                    <Input type="email" placeholder="Email" className={`w-full h-8`} ref={emailRef} />
-                    <Box border="1px solid" borderRadius="md" outline='2px' className={`pl-4 h-10 w-full flex items-center gap-2`}>
-                        <Input variant='unstyled' border='0px' placeholder="Password"
-                            type={`${showPassword ? 'text' : 'password'}`}
-                            className={`w-full h-8`}
-                            ref={passwordRef}
+                <form id="login-form" className={`h-full w-full bg-transparent gap-4 px-5 py-0 flex flex-col items-start justify-center`}
+                    onSubmit={formik.handleSubmit}
+                >
+                    {/* email */}
+                    <FormControl isInvalid={!!formik.errors.email && formik.touched.email}>
+                        <Input type="email" name="email" placeholder="Email" className={`w-full h-8`}
+                            value={formik.values.email}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
                         />
-                        {showPassword ?
-                            <FaEye size={30} className={`pr-2 text-gray-500 hover:cursor-pointer`} onClick={() => setShowPassword(false)} />
-                            : <FaEyeSlash size={30} className={`pr-2 text-gray-500 hover:cursor-pointer`} onClick={() => setShowPassword(true)} />
-                        }
-                    </Box>
+                        <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+                    </FormControl>
+
+                    {/* password */}
+                    <FormControl isInvalid={!!formik.errors.password && formik.touched.password}>
+                        <Box border="1px solid" borderRadius="md" outline='2px' className={`pl-4 h-10 w-full flex items-center gap-2`}>
+                            <Input variant='unstyled' name="password" border='0px' placeholder="Password"
+                                type={`${showPassword ? 'text' : 'password'}`}
+                                className={`w-full h-8`}
+                                value={formik.values.password}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                            />
+                            {showPassword ?
+                                <FaEye size={30} className={`pr-2 text-gray-500 hover:cursor-pointer`} onClick={() => setShowPassword(false)} />
+                                : <FaEyeSlash size={30} className={`pr-2 text-gray-500 hover:cursor-pointer`} onClick={() => setShowPassword(true)} />
+                            }
+                        </Box>
+                        <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+                    </FormControl>
 
                     {/* login button */}
                     <div className={`w-full`}>
-                        <Button rightIcon={<MdLogin size={25} />}
-                            bgGradient="linear(to-r, blue.400, blue.700)" type="button" colorScheme="blue" className={`w-full h-8 `}
+                        <Button type="submit" form="login-form" rightIcon={<MdLogin size={25} />}
+                            bgGradient="linear(to-r, blue.400, blue.700)" colorScheme="blue" className={`w-full h-8 `}
                             isLoading={loading}
-                            onClick={handleLogin}
+                            isDisabled={!formik.isValid}
                         >
                             Login
                         </Button>
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,140 +1,178 @@
-import { Box, Button, Input, useToast } from "@chakra-ui/react";
+import { v4 as uuidv4 } from 'uuid';
+import { Box, Button, FormControl, FormErrorMessage, Input, useToast } from "@chakra-ui/react";
 import { Link, useNavigate } from "react-router-dom";
 import { TiUser } from "react-icons/ti";
 import { FaEye, FaEyeSlash } from "react-icons/fa6";
-import { useRef, useState } from "react";
+import { useState } from "react";
+import { registerSchema } from "../utils/validation";
+import { useFormik } from "formik";
+import { SignupForm, SignupRequest } from "../assets/types/type";
+import { BASE_URL } from "../assets/api/api";
 
-const Register = ({ isLightmode }: { isLightmode: boolean}) => {
+const Register = ({ isLightmode }: { isLightmode: boolean }) => {
     const [showPassword, setShowPassword] = useState<boolean>(false);
     const [showConfirmPassword, setShowConfirmPassword] = useState<boolean>(false);
     const toast = useToast();
     const navigate = useNavigate();
     const [loading, setLoading] = useState<boolean>(false);
-    const firstNameRef = useRef<HTMLInputElement>(null);
-    const lastNameRef = useRef<HTMLInputElement>(null);
-    const emailRef = useRef<HTMLInputElement>(null);
-    const passwordRef = useRef<HTMLInputElement>(null);
-    const confirmPasswordRef = useRef<HTMLInputElement>(null);
 
-    // registration handler
-    const handleRegister = (e: any) => {
-        e.preventDefault();
-        // form
-        const firstName = firstNameRef.current?.value || "";
-        const lastName = lastNameRef.current?.value || "";
-        const email = emailRef.current?.value || "";
-        const password = passwordRef.current?.value || "";
-        const confirmPassword = confirmPasswordRef.current?.value || "";
-        const accountForm = {
-            firstName, lastName, email, password
-        };
-        try {
-            setLoading(true);
-            // find if already registered
-            const existsUsers = localStorage.getItem("users");
-            const users = existsUsers ? JSON.parse(existsUsers) : [];
-            const user = users.find((u: any) => u.email === email);
+    // registration
+    const formik = useFormik<SignupForm>({
+        initialValues: {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
+        },
+        validationSchema: registerSchema,
+        onSubmit: async (values, { resetForm }) => {
+            console.log("FORM SUBMITTED", values);
+            try {
+                setLoading(true);
+                // find if already registered
+                const existsUsers = await fetch(`${BASE_URL}/users?email=${values.email}`);
+                const user = await existsUsers.json();
 
-            if (!accountForm) {
-                toast({
-                    title: "Form error",
-                    description: "Fill the form correctly.",
-                    status: "error",
-                    position: 'top',
-                    duration: 3000,
-                    isClosable: true,
-                });
-            } else if (user) {
-                toast({
-                    title: "User already exists",
-                    description: "Please login instead.",
-                    status: "warning",
-                    duration: 3000,
-                    isClosable: true,
-                });
-                return;
-            } else if (password !== confirmPassword) {
-                toast({
-                    title: "Not matched",
-                    description: "Password not matched",
-                    status: "error",
-                    position: 'top',
-                    duration: 3000,
-                    isClosable: true,
-                });
-            } else {
-                // create new user
-                const newUser = {
-                    id: Date.now().toString(),
-                    email: accountForm.email,
-                    password: accountForm.password,
-                    firstName: accountForm.firstName,
-                    lastName: accountForm.lastName,
+                if (user.length > 0) {
+                    toast({
+                        title: "User already exists",
+                        description: "Please login instead.",
+                        status: "warning",
+                        duration: 3000,
+                        isClosable: true,
+                    });
+                    return;
                 }
-                localStorage.setItem("users", JSON.stringify(newUser));
+                // create new user
+                const payload: SignupRequest = {
+                    id: uuidv4(),
+                    firstName: values.firstName,
+                    lastName: values.lastName,
+                    email: values.email,
+                    password: values.password,
+                }
+                const newUser = await fetch(`${BASE_URL}/auth/register`, {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(payload),
+                });
+                if (!newUser.ok) {
+                    toast({
+                        title: "Registration Failed",
+                        status: "error",
+                        position: 'top',
+                        duration: 1000,
+                        isClosable: true,
+                    });
+                    return;
+                }
                 toast({
                     title: "Registered",
                     description: "Account created successfully.",
                     status: "success",
                     position: 'top',
-                    duration: 3000,
+                    duration: 1000,
                     isClosable: true,
                     onCloseComplete() { navigate("/login"); },
                 });
+                resetForm();
+            } catch (error: any) {
+                toast({
+                    title: "Registration Failed",
+                    description: error.message || "Something went wrong",
+                    status: "error",
+                    position: 'top',
+                    duration: 2000,
+                    isClosable: true,
+                });
+            } finally {
+                setLoading(false);
             }
-        } catch (error: any) {
-            toast({
-                title: "Registration Failed",
-                description: error.message || "Something went wrong",
-                status: "error",
-                position: 'top',
-                duration: 3000,
-                isClosable: true,
-            });
-        } finally {
-            setLoading(false);
         }
-    };
+    });
 
     return (
         <div className={`flex flex-col items-center justify-start gap-10 m-auto min-h-screen ${isLightmode ? 'bg-gray-100 text-black' : 'bg-gray-900 text-white'}`}>
             <div className={`mt-20 bg-[#73a2ce46] min-h-70 w-80 md:w-100 lg:w-100 px-5 py-4 rounded-lg flex flex-col items-center gap-4 shadow-lg`}>
                 <p className="font-semibold text-2xl">Create Account</p>
-                <form className={`h-full w-full bg-transparent gap-4 grid grid-cols-2 place-items-center`}>
-                    <Input type="text" placeholder="First name" className={`h-8`} ref={firstNameRef} />
-                    <Input type="text" placeholder="Last name" className={`h-8`} ref={lastNameRef} />
-                    <Input type="email" placeholder="Email" className={`w-full h-8 col-span-2`} ref={emailRef} />
+                <form id='register-form' className={`h-full w-full bg-transparent gap-4 grid grid-cols-2 place-items-center`}
+                    onSubmit={formik.handleSubmit}
+                >
+                    {/* first name */}
+                    <FormControl isInvalid={!!formik.errors.firstName && formik.touched.firstName}>
+                        <Input type="text" name='firstName' placeholder="First name" className={`h-8`}
+                            value={formik.values.firstName}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                        />
+                        <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
+                    </FormControl>
+
+                    {/* last name */}
+                    <FormControl isInvalid={!!formik.errors.lastName && formik.touched.lastName}>
+                        <Input type="text" name='lastName' placeholder="Last name" className={`h-8`}
+                            value={formik.values.lastName}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                        />
+                        <FormErrorMessage>{formik.errors.lastName}</FormErrorMessage>
+                    </FormControl>
+
+                    {/* email */}
+                    <FormControl gridColumn="span 2" isInvalid={!!formik.errors.email && formik.touched.email}>
+                        <Input type="email" name="email" placeholder="Email" className={`w-full h-8`}
+                            value={formik.values.email}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                        />
+                        <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+                    </FormControl>
+
                     {/* password */}
-                    <Box border="1px solid" borderRadius="md" outline='2px' className={`pl-4 h-10 flex items-center gap-2`}>
-                        <Input variant='unstyled' border='0px'
-                            type={`${showPassword ? 'text' : 'password'}`} placeholder="Password"
-                            className={`w-full h-8`}
-                            ref={passwordRef}
-                        />
-                        {showPassword ?
-                            <FaEye size={30} className={`pr-2 text-gray-500 hover:cursor-pointer`} onClick={() => setShowPassword(false)} />
-                            : <FaEyeSlash size={30} className={`pr-2 text-gray-500 hover:cursor-pointer`} onClick={() => setShowPassword(true)} />
-                        }
-                    </Box>
+                    <FormControl isInvalid={!!formik.errors.password && formik.touched.password}>
+                        <Box border="1px solid" borderRadius="md" outline='2px' className={`pl-4 h-10 w-full flex items-center gap-2`}>
+                            <Input variant='unstyled' name="password" border='0px' placeholder="Password"
+                                type={`${showPassword ? 'text' : 'password'}`}
+                                className={`w-full h-8`}
+                                value={formik.values.password}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                            />
+                            {showPassword ?
+                                <FaEye size={30} className={`pr-2 text-gray-500 hover:cursor-pointer`} onClick={() => setShowPassword(false)} />
+                                : <FaEyeSlash size={30} className={`pr-2 text-gray-500 hover:cursor-pointer`} onClick={() => setShowPassword(true)} />
+                            }
+                        </Box>
+                        <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+                    </FormControl>
+
                     {/* confirm password */}
-                    <Box border="1px solid" borderRadius="md" outline='2px' className={`pl-4 h-10 flex items-center gap-2`}>
-                        <Input variant='unstyled' border='0px'
-                            type={`${showConfirmPassword ? 'text' : 'password'}`} placeholder="Confirm Password"
-                            className={`w-full h-8`}
-                            ref={confirmPasswordRef}
-                        />
-                        {showConfirmPassword ?
-                            <FaEye size={30} className={`pr-2 text-gray-500 hover:cursor-pointer`} onClick={() => setShowConfirmPassword(false)} />
-                            : <FaEyeSlash size={30} className={`pr-2 text-gray-500 hover:cursor-pointer`} onClick={() => setShowConfirmPassword(true)} />
-                        }
-                    </Box>
+                    <FormControl isInvalid={!!formik.errors.confirmPassword && formik.touched.confirmPassword}>
+                        <Box border="1px solid" borderRadius="md" outline='2px' className={`pl-4 h-10 w-full flex items-center gap-2`}>
+                            <Input variant='unstyled' name="confirmPassword" border='0px' placeholder="Confirm"
+                                type={`${showConfirmPassword ? 'text' : 'password'}`}
+                                className={`w-full h-8`}
+                                value={formik.values.confirmPassword}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                            />
+                            {showConfirmPassword ?
+                                <FaEye size={30} className={`pr-2 text-gray-500 hover:cursor-pointer`} onClick={() => setShowConfirmPassword(false)} />
+                                : <FaEyeSlash size={30} className={`pr-2 text-gray-500 hover:cursor-pointer`} onClick={() => setShowConfirmPassword(true)} />
+                            }
+                        </Box>
+                        <FormErrorMessage>{formik.errors.confirmPassword}</FormErrorMessage>
+                    </FormControl>
                 </form>
                 {/* register button */}
-                <Button rightIcon={<TiUser size={22} />} type="button" colorScheme="blue"
+                <Button type="submit" form='register-form' rightIcon={<TiUser size={22} />} colorScheme="blue"
                     bgGradient="linear(to-l, blue.400, blue.700)"
                     className={`w-full h-8`}
                     isLoading={loading}
-                    onClick={handleRegister}
+                    isDisabled={!formik.isValid || loading}
                 >
                     Register
                 </Button>
